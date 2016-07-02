8037a0d991461353c70d0110634e70e0891c5fe9
IsaacYou <isaac_you@naver.com>
2016-07-02 11:00:21 +0900

    Wave

--- isaac_button.js
+++ isaac_button.js
@@ -2,23 +2,63 @@
 
 
 $(document).ready(function(){
-   	$(".button.wave").each(function(){
+
+   	$(".button.waves-effect").each(function(){
+   
    		var self = this;
-   		
-   		$(self).click(function(_event){
 
-            var isWaving = $(_event.target).find('.ripper')[0];
+         var downElapsedTime = 0;
+         var startTime;
+         var endTime;
 
-            if( isWaving == undefined){
+        
+   		$(self).mousedown(function(_event){
 
-      			$(_event.target).append("<div class='ripper'></div>");
-      			var ripper = $(_event.target).find('.ripper')[0];
-      			
-      			setTimeout(function(){
-   			       ripper.remove();
-   			   }, 6000);
-               
-            }
+            var isWaving = $(_event.target).find('.waves-ripple')[0];
+
+              if( isWaving == undefined){
+                  startTime = new Date();
+                
+         			$(_event.target).append("<div class='waves-ripple'></div>");
+         			var ripple = $(_event.currentTarget).find('.waves-ripple')[0];
+         			$(ripple).css({"left":_event.offsetX, "top":_event.offsetY});
+                  $(ripple).addClass('big');
+               }
+
+           // else{
+           //    _event.preventDefault();
+           // }
    		});
+
+         //다운을 오래 하고 있을 경우 
+         $(self).mouseup(function(_event){
+            console.log
+            endTime = new Date();
+            console.log(endTime - startTime);
+
+            var ripple = $(_event.currentTarget).find('.waves-ripple')[0];
+            $(ripple).remove();
+         });
+
+         //다운을 짧게 유지한 경우
+          $(self).click(function(_event){
+
+            endTime = new Date();
+            console.log(endTime-startTime);
+            var ripple = $(_event.currentTarget).find('.waves-ripple')[0];
+            $(ripple).delay(600).remove();
+
+         });
+
+
+         $(self).mouseout(function(_event){
+            var ripple = $(_event.currentTarget).find('.waves-ripple')[0];
+            $(ripple).remove();
+         })
+
+
+
+
    	})
 });
+
